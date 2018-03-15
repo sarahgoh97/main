746896e6c5e5be421a4a1b344ecb3cdf2861ca9e
philos22 <philos22@gmail.com>
2018-03-15 23:41:19 +0800

    findTags copied from https://github.com/nus-cs2103-AY1718S2/addressbook-level4/pull/94/files - seekRA command

---
+++ src/main/java/seedu/address/model/person/TagContainsKeywordsPredicate.java
@@ -0,0 +1,40 @@
+package seedu.address.model.person;
+
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+
+import seedu.address.commons.util.StringUtil;
+
+/**
+ * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
+ */
+public class TagContainsKeywordsPredicate implements Predicate<Person> {
+    private final List<String> keywords;
+
+    public TagContainsKeywordsPredicate(List<String> keywords) {
+        this.keywords = keywords;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        // Making a string of all tags
+        Set tags = person.getTags();
+        String tagNames = "";
+        for (Object tag: tags) {
+            tagNames += tag.toString() + " ";
+        }
+        final String tagList = tagNames;
+
+        return keywords.stream()
+                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(String.valueOf(person.getTags()), keyword));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof TagContainsKeywordsPredicate // instanceof handles nulls
+                && this.keywords.equals(((TagContainsKeywordsPredicate) other).keywords)); // state check
+    }
+
+}
