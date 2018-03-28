8d063fa03547b094d554825e8fe61346e17274cb
philos22 <philos22@gmail.com>
2018-03-29 04:27:18 +0800

    style 1

--- src/main/java/seedu/address/Quickstart.java
+++ src/main/java/seedu/address/Quickstart.java
@@ -1,5 +1,11 @@
 package seedu.address;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -7,19 +13,13 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.json.JsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.DateTime;
+import com.google.api.client.util.store.FileDataStoreFactory;
 
 import com.google.api.services.calendar.CalendarScopes;
 import com.google.api.services.calendar.model.*;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.List;
 
 public class Quickstart {
     /** Application name. */
@@ -98,6 +98,11 @@ public class Quickstart {
                 .build();
     }
 
+    /**
+     * iterate through events and list them out with their respective start times
+     * @return a list of upcoming events
+     * @throws IOException
+     */
     public static String Quick() throws IOException {
         // Build a new authorized API client service.
         // Note: Do not confuse this class with the
